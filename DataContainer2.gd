6aff312576c083584f10c560ce78f04510e72f09
sericaer <sericaer@outlook.com>
2021-04-02 10:41:54 +0800

    modify error

--- DataContainer2.gd
+++ DataContainer2.gd
@@ -3,8 +3,7 @@ tool
 extends PanelContainer
 
 func show_rows(table_data, template_path):
-	var labels : Array = table_data.get_lables(template_path) 
-	$DataContainer.show_rows(labels)
+	$DataContainer.show_rows(table_data.get_lables(template_path) )
 	$RowButtonContainer.set_rows(table_data.get_buttons())
 
 func set_column_size(size):
--- RowButtonContainer.gd
+++ RowButtonContainer.gd
@@ -3,10 +3,8 @@ tool
 extends VBoxContainer
 
 func set_rows(buttons:Array):
+	for child in self.get_children():
+		self.remove_child(child)
 	for button in buttons:
 		if button.get_parent() == null:
 			add_child(button)
-
-func clear_rows():
-	for child in self.get_children():
-		self.remove_child(child)
