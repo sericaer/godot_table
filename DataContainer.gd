adcac7c416b8dd4e95c1cd7d6c65abb0ca3e58c6
Tim <zschuppetim@gmail.com>
2024-06-25 02:00:46 +0200

    Initial release
    Update to Godot 4
    added custom StyleBoxes for the header
    added custom and different StyleBoxes for even and odd rows
    added custom fonts and font size

--- DataContainer.gd
+++ DataContainer.gd
@@ -1,8 +1,12 @@
-tool
+@tool
+
 extends GridContainer
 
-export var template_path = ""
-export var datas = []
+@export var template_path = ""
+@export var datas = []
+
+var main_font:Font
+var font_size:int = 1
 
 func clear_rows():
 	for child in self.get_children():
@@ -17,15 +21,23 @@ func set_rows(rows, column_size):
 			add_new_table_data_item(row_index, col_index, data)
 
 func add_new_table_data_item(row, column, data):
-	var data_obj = load(template_path).instance()
+	var data_obj = load(template_path).instantiate()
 	data_obj.name = "DATA_%d_%d"%[row, column]
 	data_obj.text = String(data)
+	
+	data_obj.add_theme_font_override("font", main_font)
+	data_obj.add_theme_font_size_override("font_size", font_size)
+	
 	self.add_child(data_obj)
 
 func sort_column(cmp):
-	datas.sort_custom(cmp, "sort")
+	datas.sort_custom(Callable(cmp,"sort"))
 	clear_rows()
 	set_rows(datas, self.columns)
 
 func get_row_data(index):
 	return datas[index]
+
+func set_font(main_font:Font, font_size:int) -> void:
+	self.main_font = main_font
+	self.font_size = font_size
--- plugin.gd
+++ plugin.gd
@@ -1,10 +1,11 @@
-tool
+@tool
+
 extends EditorPlugin
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
 	# Add the new type with a name, a parent type, a script and an icon.
-	add_custom_type("Table", "PanelContainer", preload("Table.gd"), preload("icon.svg"))
+	add_custom_type("Table", "PanelContainer", preload("res://addons/godot_table/Table.gd"), preload("res://addons/godot_table/icon.svg"))
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
